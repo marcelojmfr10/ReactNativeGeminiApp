837d54f01dc962bc14e1d5c0e93d942fc267e1cb
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-08-30 18:49:38 -0600

    Estructura de proyecto con páginas del chat

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import * as eva from "@eva-design/eva";
+import { Slot, SplashScreen } from "expo-router";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { ApplicationProvider } from "@ui-kitten/components";
+import { Fragment } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Fragment>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Slot />
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </ApplicationProvider>
+    </Fragment>
   );
 }
