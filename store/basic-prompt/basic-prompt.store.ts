a1db97d58e16a6d2dc4ce575eeed883a5dd94582
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-09-01 18:57:55 -0600

    Funcionalidad de stream

--- store/basic-prompt/basic-prompt.store.ts
+++ store/basic-prompt/basic-prompt.store.ts
@@ -1,4 +1,4 @@
-import * as GeminiActions from "@/actions/gemini/basic-prompt.action";
+import * as GeminiActions from "@/actions/gemini";
 import { Message } from "@/interfaces/chat.interfaces";
 import uuid from "react-native-uuid";
 import { create } from "zustand";
@@ -6,7 +6,7 @@ import { create } from "zustand";
 interface BasicPromptState {
   geminiWriting: boolean;
   messages: Message[];
-  addMessage: (text: string) => void;
+  addMessage: (prompt: string) => void;
   setGeminiWriting: (isWriting: boolean) => void;
 }
 
@@ -23,19 +23,27 @@ const createMessage = (text: string, sender: "user" | "gemini"): Message => {
 export const useBasicPromptStore = create<BasicPromptState>()((set) => ({
   geminiWriting: false,
   messages: [],
-  addMessage: async (text: string) => {
-    const userMessage = createMessage(text, "user");
+  addMessage: async (prompt: string) => {
+    const userMessage = createMessage(prompt, "user");
+    const geminiMessage = createMessage("Generando respuesta...", "gemini");
     set((state) => ({
       geminiWriting: true,
-      messages: [userMessage, ...state.messages],
+      messages: [geminiMessage, userMessage, ...state.messages],
     }));
 
-    const geminiResponseText = await GeminiActions.getBasicPrompt(text);
-    const geminiMessage = createMessage(geminiResponseText, "gemini");
-    set((state) => ({
-      geminiWriting: false,
-      messages: [geminiMessage, ...state.messages],
-    }));
+    // const geminiResponseText = await GeminiActions.getBasicPrompt(text);
+    // const geminiMessage = createMessage(geminiResponseText, "gemini");
+    // set((state) => ({
+    //   geminiWriting: false,
+    //   messages: [geminiMessage, ...state.messages],
+    // }));
+    GeminiActions.getBasicPromptStream(prompt, (text) => {
+      set((state) => ({
+        messages: state.messages.map((msg) =>
+          msg.id === geminiMessage.id ? { ...msg, text } : msg,
+        ),
+      }));
+    });
   },
   setGeminiWriting: (isWriting: boolean) => {
     set({ geminiWriting: isWriting });
